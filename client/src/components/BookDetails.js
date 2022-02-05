944e233cde0df9109c1c057f7df3da1325e35cd2
baoanh1310 <dataholicguy@gmail.com>
2022-02-06 00:51:34 +0700

    connect UI to graphql server sucess

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Card from 'react-bootstrap/Card';
+import { useQuery } from '@apollo/client';
 
-const BookDetails = () => {
+import { getSingleBook } from '../graphql-client/queries';
+
+const BookDetails = ({bookId}) => {
+
+    const {loading, error, data} = useQuery(getSingleBook, {
+        variables: {
+            id: bookId
+        },
+        skip: bookId === null,
+    });
+
+    if (loading) return <p>Loading book details...</p>
+    if (error) return <p>Error loading book details!</p>
+
+    const book = bookId !== null ? data.book : null;
+
     return (
         <Card bg='info' text='white' className='shadow'>
             <Card.Body>
-                <Card.Title>Mastering Ethereum</Card.Title>
-                <Card.Subtitle>Blockchain</Card.Subtitle>
-                <p>Vitalik Buterin</p>
-                <p>Age: 25</p>
-                <p>All books by this author</p>
-                <ul>
-                    <li>Mastering Ethereum</li>
-                    <li>Ethereum 2.0</li>
-                </ul>
+                {
+                    book === null ? <Card.Text>Please select a book!</Card.Text> : 
+                    <Fragment>
+                        <Card.Title>{book.name}</Card.Title>
+                        <Card.Subtitle>{book.genre}</Card.Subtitle>
+                        <p>{book.author.name}</p>
+                        <p>Age: {book.author.age}</p>
+                        <p>All books by this author</p>
+                        <ul>
+                            {book.author.books.map(book => (
+                                <li key={book.id}>{book.name}</li>
+                            ))}
+                        </ul>
+                    </Fragment>
+                }
             </Card.Body>
         </Card>
     );
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,41 +1,44 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card';
-import CardColumns from 'react-bootstrap/CardColumns';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import React, { useState } from 'react'
+import Card from 'react-bootstrap/Card'
+import CardColumns from 'react-bootstrap/CardColumns'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import BookDetails from './BookDetails'
 
-import BookDetails from './BookDetails';
+import { useQuery } from '@apollo/client'
+import { getBooks } from '../graphql-client/queries'
 
 const BookList = () => {
-  return (
-        <Row>
-            <Col xs={8}>
-                <CardColumns>
-                    <Card border='info' text='info' className='text-center shadow'>
-                        <Card.Body>Mastering Bitcoin</Card.Body>
-                    </Card>
-                    <Card border='info' text='info' className='text-center shadow'>
-                        <Card.Body>Mastering Bitcoin</Card.Body>
-                    </Card>
-                    <Card border='info' text='info' className='text-center shadow'>
-                        <Card.Body>Mastering Bitcoin</Card.Body>
-                    </Card>
-                    <Card border='info' text='info' className='text-center shadow'>
-                        <Card.Body>Mastering Bitcoin</Card.Body>
-                    </Card>
-                    <Card border='info' text='info' className='text-center shadow'>
-                        <Card.Body>Mastering Bitcoin</Card.Body>
-                    </Card>
-                    <Card border='info' text='info' className='text-center shadow'>
-                        <Card.Body>Mastering Bitcoin</Card.Body>
-                    </Card>
-                </CardColumns>
-            </Col>
-            <Col>
-                <BookDetails />
-            </Col>
-        </Row>
-    );
-};
+	const [bookSelected, setBookSelected] = useState(null)
 
-export default BookList;
+	const { loading, error, data } = useQuery(getBooks)
+
+	if (loading) return <p>Loading books....</p>
+	if (error) return <p>Error loading books!</p>
+
+	return (
+		<Row>
+			<Col xs={8}>
+				<CardColumns>
+					{data.books.map(book => (
+						<Card
+							border='info'
+							text='info'
+							className='text-center shadow'
+							key={book.id}
+							onClick={setBookSelected.bind(this, book.id)}
+							style={{ cursor: 'pointer' }}
+						>
+							<Card.Body>{book.name}</Card.Body>
+						</Card>
+					))}
+				</CardColumns>
+			</Col>
+			<Col>
+				<BookDetails bookId={bookSelected} />
+			</Col>
+		</Row>
+	)
+}
+
+export default BookList
